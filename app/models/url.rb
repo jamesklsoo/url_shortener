bbb0df1d760df4880e02ec6d022b310c153976dc
James Soo <jamesklsoo@gmail.com>
2017-07-05 14:45:15 +0800

    update controller and model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,16 @@
+require 'uri'
 class Url < ApplicationRecord
+  before_create :shorten, :counter
+  validates :long_url, :presence => true, :format => URI::regexp(%w(http https))
+
+  def shorten
+    range = [*'0'..'9', *'A'..'Z', *'a'..'z']
+
+    @short_url = (0..6).map{ range.sample }.join
+    self.short_url = @short_url
+  end
+
+  def counter
+    self.click_count = 0
+  end
 end
